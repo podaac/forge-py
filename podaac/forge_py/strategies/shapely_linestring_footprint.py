2eda991e5996d87c2d03b0a39d16594febe8e582
henze <henze@jpl.nasa.gov>
2025-10-06 11:22:14 -0700

    Small poetry mods.

--- podaac/forge_py/strategies/shapely_linestring_footprint.py
+++ podaac/forge_py/strategies/shapely_linestring_footprint.py
@@ -33,19 +33,19 @@ def fit_footprint(lon, lat, simplify=0.9, max_dist=None, **kwargs):
         lon_segments, lat_segments = [lon], [lat]
     else:
         lon_segments, lat_segments = split_path_maxdist(lon, lat, max_dist)
-    
+
     # Package as LineString objects:
     points_segments = []  # Hold LineString objects for each segment in here.
     for lonseg, latseg in zip(lon_segments, lat_segments):
         points_segments.append(LineString([(x, y) for x, y in zip(lonseg, latseg)]))
 
     # Fit, either whole path or each segment if there was splitting:
-    fit = [ shapely.simplify(pts, tolerance=simplify) for pts in points_segments ]
-    
+    fit = [shapely.simplify(pts, tolerance=simplify) for pts in points_segments]
+
     # Split any footprint segments at international dateline crossings.
-    # During this process there is an awkward recasting of paths from LineString 
+    # During this process there is an awkward recasting of paths from LineString
     # objs to a list of np.arrays, which accounts for most of the code below:
-    fit_idlsplit = [[],[]]  # First element for lons, 2nd element for lats.
+    fit_idlsplit = [[], []]  # First element for lons, 2nd element for lats.
     for seg in fit:
         lons_split_idl, lats_split_idl = split_path_idl(seg.xy[0], seg.xy[1])
         fit_idlsplit[0] = fit_idlsplit[0] + lons_split_idl
@@ -61,9 +61,9 @@ def fit_footprint(lon, lat, simplify=0.9, max_dist=None, **kwargs):
 def split_path_idl(lons, lats):
     """
     Splits a 1D latitude, longitude path over any international dateline (IDL) crossings.
-    Inputs lon, lat should represent the entire path as 1D numpy arrays with the same length 
-    and ordered along the path. Longitudes should have the domain [-180, 180). Returns two 
-    lists of numpy arrays, one each for lons and lats. Each numpy array represents the lons 
+    Inputs lon, lat should represent the entire path as 1D numpy arrays with the same length
+    and ordered along the path. Longitudes should have the domain [-180, 180). Returns two
+    lists of numpy arrays, one each for lons and lats. Each numpy array represents the lons
     or lats for one of the path segments.
     """
     # Find indices where longitude difference is >= 360 between subsequent points.
@@ -80,15 +80,15 @@ def split_path_idl(lons, lats):
 
     else:  # if no splitting needed, return lons, lats as single arrays:
         return [lons], [lats]
-    
+
 
 def split_path_maxdist(lons, lats, max_dist):
     """
     Splits a 1D latitude, longitude path into segments at pairs of points that are
     further apart than a threshold distance (computed as the haversine great circle distance).
-    Inputs lon, lat should represent the entire path as 1D numpy arrays with the same length 
-    and ordered along the path. max_dist is the threshold distance (float). Returns two 
-    lists of numpy arrays, one each for lons and lats. Each numpy array represents the lons 
+    Inputs lon, lat should represent the entire path as 1D numpy arrays with the same length
+    and ordered along the path. max_dist is the threshold distance (float). Returns two
+    lists of numpy arrays, one each for lons and lats. Each numpy array represents the lons
     or lats for one of the path segments.
     """
     # Compute haversine distance and get the index for values grater than threshold:
@@ -97,23 +97,23 @@ def split_path_maxdist(lons, lats, max_dist):
 
     # Split arrays if needed:
     if len(i_gt_max) > 0:
-        # Append first/last index for easier indexing. -1 is used instead of 0 for the first index 
+        # Append first/last index for easier indexing. -1 is used instead of 0 for the first index
         # so that when +1 is added below it will return to 0:
         i_gt_max = [-1] + list(i_gt_max) + [len(havdist)]
         lon_segs = []
         lat_segs = []
         for j in range(len(i_gt_max)-1):
-            lon_segs.append(lons[i_gt_max[j] + 1 : i_gt_max[j+1] + 1])  # +1 translates to needed index in lons.
-            lat_segs.append(lats[i_gt_max[j] + 1 : i_gt_max[j+1] + 1])  # +1 translates to needed index in lats.
+            lon_segs.append(lons[i_gt_max[j] + 1: i_gt_max[j+1] + 1])  # +1 translates to needed index in lons.
+            lat_segs.append(lats[i_gt_max[j] + 1: i_gt_max[j+1] + 1])  # +1 translates to needed index in lats.
         return lon_segs, lat_segs
     else:
         return [lons], [lats]
-    
+
 
 def haversine_distance(lon1, lat1, lon2, lat2, r_earth=6378.137):
     """
-    Computes the haversine great circle distance between two points in km's. Input latitude and longitudes 
-    should be in degrees and numpy array-like or floats. Default is to assume radius of Earth at equator. 
+    Computes the haversine great circle distance between two points in km's. Input latitude and longitudes
+    should be in degrees and numpy array-like or floats. Default is to assume radius of Earth at equator.
     r_earth should be in km's.
     """
     lon1 = np.deg2rad(lon1)  # Convert to radians
@@ -124,5 +124,5 @@ def haversine_distance(lon1, lat1, lon2, lat2, r_earth=6378.137):
     # Compute chord length, central angle, and great circle distance:
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    chord_len = 2*( np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2)**2 )**0.5
+    chord_len = 2*(np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2)**2)**0.5
     return r_earth*2*np.arcsin(chord_len/2)
